b3dcf4edd0df1e80b3829ba8f59d7d51481f9e22
Axel Teo <teojunda@gmail.com>
2026-02-24 02:27:23 +0800

    Return to what works

--- src/sqlancer/Main.java
+++ src/sqlancer/Main.java
@@ -181,9 +181,9 @@ public final class Main {
         }
 
         public FileWriter getCurrentFileWriter() {
-            // if (!logEachSelect) {
-            //     throw new UnsupportedOperationException();
-            // }
+            if (!logEachSelect) {
+                throw new UnsupportedOperationException();
+            }
             if (currentFileWriter == null) {
                 try {
                     currentFileWriter = new FileWriter(curFile, false);
@@ -514,8 +514,10 @@ public final class Main {
                     reproducer = provider.generateAndTestDatabase(state);
                 }
                 try {
-                    logger.getCurrentFileWriter().close();
-                    logger.currentFileWriter = null;
+                    if (logger.logEachSelect) {
+                        logger.getCurrentFileWriter().close();
+                        logger.currentFileWriter = null;
+                    }
                 } catch (IOException e) {
                     throw new AssertionError(e);
                 }
